85108970cf7331bfb6be27c5b29b341814ba7a02
Georgie Brown <georginaabrown@gmail.com>
2020-05-29 13:46:25 +1000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :show, :create, :new]
-
+  resources :cocktails, only: [:index, :show, :create, :new, :destroy] do
+    resources :doses, only: [:new, :create] do
+  end
+  resources :doses, only: [:destroy]
+end
 end
